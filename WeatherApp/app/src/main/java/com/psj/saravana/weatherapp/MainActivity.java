2c53baea6da354766d10a47b1a7c267d5f73c636
Subramaniam, S. E <s.e.subramaniam@accenture.com>
2015-06-20 23:24:17 +0530

    btn layout xml files created

--- WeatherApp/app/src/main/java/com/psj/saravana/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/psj/saravana/weatherapp/MainActivity.java
@@ -1,17 +1,30 @@
 package com.psj.saravana.weatherapp;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText cityET=(EditText) findViewById(R.id.etCityName);
+        Button btn=(Button) findViewById(R.id.btnGetWeather);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Btn Clicked",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
